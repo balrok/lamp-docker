2d84f5b07fabe7e59312803141db4223189f6eb4
Carl Mai <carl.schoenbach@gmail.com>
2021-10-23 21:13:17 +0200

    fix dockerfile

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -4,15 +4,15 @@ LABEL author="Carl Mai"
 EXPOSE 9000
 
 RUN apt-get update &&\
-	apt-get install -y\
+	apt-get install -y --no-install-recommends\
 	php7.3-fpm \
 	php7.3-bcmath php7.3-curl php7.3-gd \
 	php7.3-json php7.3-mbstring php7.3-mysql \
 	php7.3-opcache php7.3-phpdbg php7.3-readline php7.3-xml php7.3-zip && \
 	rm -rf /var/lib/apt/lists/*
 
-RUN sed -i 's/^listen.*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
-RUN mkdir -p /run/php
+RUN sed -i 's/^listen.*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf && \
+	mkdir -p /run/php
 
 
 CMD [ "php-fpm7.3", "-F" ]
