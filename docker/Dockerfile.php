14ddb7f6f7c8b1c55d6f5d8e04f3a16ec3279fe8
Carl Mai <carl.schoenbach@gmail.com>
2021-12-13 21:53:47 +0100

    mail support

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -8,8 +8,15 @@ RUN apt-get update &&\
         php7.3-fpm \
         php7.3-bcmath php7.3-curl php7.3-gd \
         php7.3-json php7.3-mbstring php7.3-mysql \
-        php7.3-opcache php7.3-phpdbg php7.3-readline php7.3-xml php7.3-zip && \
+        php7.3-opcache php7.3-phpdbg php7.3-readline php7.3-xml php7.3-zip \
+        ca-certificates \
+        msmtp mailutils && \
         rm -rf /var/lib/apt/lists/*
+
+
+COPY ./msmtprc /etc/msmtprc
+RUN echo "sendmail_path=/usr/bin/msmtp -t" >> /etc/php/7.3/fpm/php.ini
+#RUN echo "localhost localhost.localdomain" >> /etc/hosts
 
 RUN sed -i 's/^listen.*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf && \
         mkdir -p /run/php
